5c22e0f6c95eb2a7c648574d0fcaf17f55e3de19
ankitjha-webdev <190101120091@cutm.ac.in>
2021-10-27 17:08:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-function App() {
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Services from './pages/Service/Services';
+import Testimonial from './pages/Testimonial/Testimonial';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <Navbar/>
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/about" exact>
+          <About/>
+        </Route>
+        <Route path="/service" exact>
+          <Services/>
+        </Route>
+        <Route path="/testimonial" exact>
+          <Testimonial/>
+        </Route>
+        <Route path="/contact" exact>
+          <Contact/>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </main>
+   </Router>
   );
 }
 
